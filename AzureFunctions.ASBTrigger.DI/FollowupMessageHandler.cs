c1c6f94b8e1aa4e591d35c2d0a9c9f9e4c772075
Sean Feldman <feldman.sean@gmail.com>
2020-10-08 18:38:09 -0600

    Attempt to repro

---
+++ AzureFunctions.ASBTrigger.DI/FollowupMessageHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region FollowupMessageHandler
+
+public class FollowupMessageHandler : IHandleMessages<FollowupMessage>
+{
+    private readonly IMyService myService;
+    private static readonly ILog Log = LogManager.GetLogger<FollowupMessageHandler>();
+
+    public FollowupMessageHandler(IMyService myService)
+    {
+        this.myService = myService;
+    }
+
+    public Task Handle(FollowupMessage message, IMessageHandlerContext context)
+    {
+        Log.Warn($"Handling {nameof(FollowupMessage)} in {nameof(FollowupMessageHandler)}. Service says: {myService.SayHello()}");
+        return Task.CompletedTask;
+    }
+}
+
+#endregion
---
+++ AzureFunctions.ASBTrigger.DI/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using NServiceBus;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+public class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddScoped(typeof(MyService));
+        builder.Services.AddScoped<IMyService>(sp => sp.GetRequiredService<MyService>());
+
+        builder.Services.AddSingleton(sp => new FunctionEndpoint(executionContext =>
+        {
+            var configuration = ServiceBusTriggeredEndpointConfiguration.FromAttributes();
+
+            configuration.UseSerialization<NewtonsoftSerializer>();
+
+            configuration.LogDiagnostics();
+
+            configuration.AdvancedConfiguration.UseContainer(new CustomServiceProviderFactory(builder.Services));
+
+            return configuration;
+        }));
+    }
+}
+
+public class MyService : IMyService
+{
+    public string SayHello() => "Hello!";
+}
+
+public interface IMyService
+{
+    string SayHello();
+}
