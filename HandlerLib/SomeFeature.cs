a083213d8370e6186ae66df8755149fb35827dc0
Tim Bussmann <timbussmann@users.noreply.github.com>
2020-11-06 17:51:57 +0100

    add handler project to test external handlers

---
+++ HandlerLib/SomeFeature.cs
@@ -0,0 +1,20 @@
+﻿using NServiceBus.Features;
+using NServiceBus.Logging;
+
+namespace HandlerLib
+{
+    public class SomeFeature : Feature
+    {
+        private ILog logger = LogManager.GetLogger<SomeFeature>();
+
+        public SomeFeature()
+        {
+            EnableByDefault();
+        }
+
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            logger.Info($"{nameof(SomeFeature)} setup invoked");
+        }
+    }
+}
---
+++ HandlerLib/SomeHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace HandlerLib
+{
+    public class SomeHandler : IHandleMessages<object>
+    {
+        private static ILog logger = LogManager.GetLogger<SomeHandler>();
+
+        public Task Handle(object message, IMessageHandlerContext context)
+        {
+            logger.Info($"{nameof(SomeHandler)} was invoked for message {message.GetType()}");
+            return Task.CompletedTask;
+        }
+    }
+}
